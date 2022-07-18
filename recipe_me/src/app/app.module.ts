2a4ef0775e68fad569c35554d2107c5bb6c33301
Encryptabit <jacari.jones@live.com>
2022-07-17 18:27:27 -0700

    initial component scaffholding

--- recipe_me/src/app/app.module.ts
+++ recipe_me/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeListItemComponent } from './components/recipe-list/recipe-list-item/recipe-list-item.component';
+import { RecipeDetailComponent } from './components/recipe-list/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeListItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
